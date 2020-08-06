b2a18026491551b569fdca3d0cc71ebcefcc90e9
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-06 15:25:25 -0400

    .env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require("./server.js");
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log("server running on port 5000");
 });
