2e87d55654792209142533558b0f49758c8e2e19
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-06 11:50:54 -0400

    postRouter!!

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,42 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Posts = require("./postDb");
+router.get("/", (req, res) => {
+  Posts.get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch();
+});
+router.get("/:id", (req, res) => {
+  Posts.getById(req.params.id)
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch();
+});
+router.delete("/:id", validatePostId, (req, res) => {
+  Posts.remove(req.params.id)
+    .then((removedPost) => {
+      res.status(200).json({
+        message: `records removed ${removedPost}, post id removed ${req.params.id}`,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "cannot remove post" });
+    });
+});
+router.put("/:id", (req, res) => {
   // do your magic!
 });
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
-
 // custom middleware
-
 function validatePostId(req, res, next) {
-  // do your magic!
+  Posts.getById(req.params.id).then((post) => {
+    if (post) {
+      next();
+    } else {
+      res.status(404).json({ message: "cannot find post " });
+    }
+  });
 }
-
 module.exports = router;
